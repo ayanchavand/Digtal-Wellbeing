939e189e587f600f7dd2d8e53791017d9b043d39
ayanchavand <ayan.chavand15@gmail.com>
2023-09-27 17:28:18 +0530

    Added Screentime feature

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
 #include <Windows.h>
 
+SYSTEMTIME timeDifference(SYSTEMTIME a) {
+	
+	SYSTEMTIME endTime, diff;
+	GetLocalTime(&endTime);
+
+	int minDiff = endTime.wMinute - a.wMinute;
+	int hourDiff = endTime.wHour - a.wHour;
+	diff.wMinute = minDiff;
+	diff.wHour = hourDiff;
+
+	return diff;
+	
+}
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+
+	int SyncTime = 60;
+	SYSTEMTIME startTime, screenTime;
+	GetLocalTime(&startTime);
+
+	while (true)
+	{	
+		std::cout << "######################" << std::endl;
+		std::cout << "DIGITAL WELLBEING" << std::endl;
+		std::cout << "######################\n\n";
+		std::cout << "Session started at: " << startTime.wHour << ":" << startTime.wMinute << std::endl;
+		screenTime = timeDifference(startTime);
+		std::cout << "Screen Time:" << screenTime.wHour << ":" << screenTime.wMinute << std::endl;
+		Sleep(SyncTime * 1000);
+		system("cls");
+		
+	}
+	
 	return 0;
 }
