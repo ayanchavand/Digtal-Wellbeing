b07b4a0159240fc91f7a67f18639477fadacc101
ayanchavand <ayan.chavand15@gmail.com>
2023-09-28 17:50:28 +0530

    Screen timer improved, Added Time class

--- main.cpp
+++ main.cpp
@@ -1,35 +1,81 @@
 #include <iostream>
+#include <fstream>
 #include <Windows.h>
 
-SYSTEMTIME timeDifference(SYSTEMTIME a) {
-	
-	SYSTEMTIME endTime, diff;
-	GetLocalTime(&endTime);
 
-	int minDiff = endTime.wMinute - a.wMinute;
-	int hourDiff = endTime.wHour - a.wHour;
-	diff.wMinute = minDiff;
-	diff.wHour = hourDiff;
+class Time {
+public:
+	int hour = 0;
+	int minute = 0;
+	int second = 0;
 
-	return diff;
-	
+	void printTime() {
+		std::cout << hour << ":" << minute << ":" << second << std::endl;
+	}
+
+	void incrementTime() {
+		second++;
+		if (second == 60) {
+			second = 0;
+			minute++;
+			if(minute == 60){
+				minute = 0;
+				hour++;
+			}
+		}
+
+	}
+};
+
+bool fileExists(const char* filename) {
+	std::ifstream infile(filename);
+	return infile.good();
+}
+
+int saveData(Time time) {
+	std::ofstream outfile("data.bin", std::ios::binary);
+	if (outfile.is_open()) {
+		// Write the array to the file
+		outfile.write(reinterpret_cast<char*>(&time), sizeof(time));
+		return 0;
+	}
+		// Close the file
+	outfile.close();
+}
+
+
+Time retrieveData() {
+	std::ifstream infile("data.bin", std::ios::binary);
+
+	if (infile.is_open()) {
+		// Create an object of MyClass to store the read data
+		Time time;
+		infile.read(reinterpret_cast<char*>(&time), sizeof(time));
+		infile.close();
+		return time;
+	}
 }
 
 int main() {
 
-	int SyncTime = 60;
-	SYSTEMTIME startTime, screenTime;
-	GetLocalTime(&startTime);
+	Time ScreenTime;
+	int SyncTime = 1;
+
+	bool FileExists = fileExists("data.bin");
+	if (FileExists) {
+		ScreenTime = retrieveData();
+	}
 
 	while (true)
 	{	
 		std::cout << "######################" << std::endl;
 		std::cout << "DIGITAL WELLBEING" << std::endl;
 		std::cout << "######################\n\n";
-		std::cout << "Session started at: " << startTime.wHour << ":" << startTime.wMinute << std::endl;
-		screenTime = timeDifference(startTime);
-		std::cout << "Screen Time:" << screenTime.wHour << ":" << screenTime.wMinute << std::endl;
+		std::cout << "Screen Time:";
+		ScreenTime.printTime();
 		Sleep(SyncTime * 1000);
+		ScreenTime.incrementTime();
+		saveData(ScreenTime);
 		system("cls");
 		
 	}
